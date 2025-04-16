6f2817b2ebb7d26c73090be1fd0eabbb19e57252
HeH-Gianni <gianni.tricarico@heh.be>
2025-04-16 14:13:50 +0200

    Ajout reactrouter dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { reactRouter } from "@react-router/dev/vite";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin()],
+  plugins: [react(), netlifyPlugin(), reactRouter()],
   test: {
     environment: "jsdom",
     setupFiles: './src/test/setup.js',
