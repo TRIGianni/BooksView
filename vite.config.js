bf12b87f889b3ce0ed9026dedf845bca12daa6e6
HeH-Gianni <gianni.tricarico@heh.be>
2025-04-16 14:40:19 +0200

    enlever les fichiers pour Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { reactRouter } from "@react-router/dev/vite";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin(), reactRouter()],
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: './src/test/setup.js',
