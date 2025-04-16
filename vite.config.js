1ec807b3071c428fffbad0a5ddf7eefddfa32fdc
HeH-Gianni <gianni.tricarico@heh.be>
2025-04-16 13:47:26 +0200

    Ajout plugin netlify pour le routage

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyPlugin()],
   test: {
     environment: "jsdom",
-    setupFiles: './src/test/setup.js'
+    setupFiles: './src/test/setup.js',
   }
 })
